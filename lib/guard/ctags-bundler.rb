669fc035fcf1092464abf41e949e1abb06490dea
Alexey Artamonov <i@useruby.com>
2016-01-03 17:43:36 +0100

    skip regenerating bundler tags if bundler_tags if false

--- lib/guard/ctags-bundler.rb
+++ lib/guard/ctags-bundler.rb
@@ -20,7 +20,7 @@ module Guard
     end
 
     def run_on_changes(paths)
-      if paths.include?('Gemfile.lock')
+      if paths.include?('Gemfile.lock') && options[:bundler_tags]
         UI.info "regenerating bundler tags..." unless @silent
         @ctags_generator.generate_bundler_tags
       end
